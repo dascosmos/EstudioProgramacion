1d1929d685d848838de14c68c4aecfd94ee45d8f
davidsalazar <david.salazar033@outlook.com>
2018-01-29 17:03:54 -0500

    Añadir clase para ejercicio left area

--- src/LeftArea.java
+++ src/LeftArea.java
@@ -2,45 +2,62 @@ import java.util.ArrayList;
 
 public class LeftArea {
 
-    public void leftArea(char o){
+    public void leftArea(char o) {
         int filas = 0;
         int columnas = 0;
         double suma = 0;
         double avg = 0;
+        int tamanioA=0;
+        boolean flag = true;
         ArrayList<Double> numerosAvg = new ArrayList<Double>();
-        double [][] matriz = new double[12][12];
-
-        for(filas = 0;filas < matriz.length;filas++){
-            for (columnas = 0; columnas < matriz[filas].length;columnas++){
-                matriz[filas][columnas]= columnas;
-                System.out.print(matriz[filas][columnas]);
+        double[][] matriz = new double[12][12];
+        int mitad = (matriz[filas].length / 2);
+        int cont = mitad-1;
+        for (filas = 0; filas < matriz.length; filas++) {
+            for (columnas = 0; columnas < matriz[filas].length; columnas++) {
+                matriz[filas][columnas] = columnas;
+                System.out.print("|" + matriz[filas][columnas] + "|");
             }
             System.out.println();
         }
 
-        for(filas = 0;filas < matriz.length;filas++){
-            for (columnas = 0; columnas < matriz[filas].length;columnas++){
-                if((filas < columnas)&&(columnas<(matriz[filas].length/2)-1)){
-                    switch(o){
-                        case 'S':
-                            suma += matriz[filas][columnas];
-                            break;
-                        case 'M':
-                            numerosAvg.add(matriz[filas][columnas]);
-                            break;
-                        default:
-                            System.out.println("Error");
-                            break;
+        for (filas = 0; filas < matriz.length; filas++) {
+            for (columnas = 0; columnas < matriz[filas].length; columnas++) {
+                if (flag) {
+                    if ((columnas < filas) && (columnas < (mitad-1))) {
+                        suma += matriz[filas][columnas];
+                        tamanioA++;
+
                     }
+                    if (filas == mitad) {
+                        flag = false;
+                    }
+                } else {
+
+                    if(columnas == cont) {
+                        cont--;
+                        System.out.println("contador" + cont);
+                    }
+
+                    if (columnas < cont) {
+                        suma += matriz[filas][columnas];
+                        tamanioA++;
+                    }
+
+
                 }
             }
+
         }
 
-        for (int i = 1; i<numerosAvg.size();i++){
-            avg = (numerosAvg.get(i)+numerosAvg.get(i-1))/numerosAvg.size();
-        }
+        avg = suma/tamanioA;
 
-        System.out.println("Suma: "+suma);
-        System.out.println("Avg: "+avg);
+        if(o == 'S'){
+            System.out.println("Suma: " + suma);
+        }else if(o == 'M'){
+            System.out.println("Avg: " + avg);
+        }else{
+            System.out.println("Abrance");
+        }
     }
 }
